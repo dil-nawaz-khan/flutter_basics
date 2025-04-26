7aebf253f1020971c7d6ed68a909760197e79af2
Dilnawaz Khan <dilnawaz@tentwenty.me>
2025-04-26 21:44:51 +0500

    Add GradientContainer widget for improved UI structure

---
+++ lib/gradient_container.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  // const GradientContainer({key}) : super(key: key);
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Colors.deepPurpleAccent, Colors.deepPurple],
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_basics/gradient_container.dart';
+
 void main() =>
     runApp(const MaterialApp(home: Scaffold(body: GradientContainer())));
-
-class GradientContainer extends StatelessWidget {
-  // const GradientContainer({key}) : super(key: key);
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [Colors.deepPurpleAccent, Colors.deepPurple],
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
-  }
-}

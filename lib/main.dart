22852563f677a6acd1dd4b514b81eb0fb52d4130
Dilnawaz Khan <dilnawaz@tentwenty.me>
2025-04-26 21:43:44 +0500

    Refactor main function to use GradientContainer widget for better structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-  MaterialApp(
-    home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [Colors.deepPurpleAccent, Colors.deepPurple],
-          ),
+void main() =>
+    runApp(const MaterialApp(home: Scaffold(body: GradientContainer())));
+
+class GradientContainer extends StatelessWidget {
+  // const GradientContainer({key}) : super(key: key);
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Colors.deepPurpleAccent, Colors.deepPurple],
         ),
-        child: const Center(
-          child: Text(
-            'Hello World!',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-            ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
           ),
         ),
       ),
-    ),
-  ),
-);
+    );
+  }
+}
